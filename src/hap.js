a0fe789ae7afdec4cbe12f99978b39778147d43e
Happyworm <markp@happyworm.com>
2014-01-18 22:21:25 +0000

    Fixed title duration value display code.

--- src/hap.js
+++ src/hap.js
@@ -175,7 +175,7 @@ HAP.init = (function (window, document) {
 				}
 				el.setAttribute('data-effect', 'title');
 				el.className += ' effect';
-				el.innerHTML = '<form onsubmit="return false"><label>Title: <span class="value">1</span>s</label><input id="effect-title" type="text" value="Title" onchange="this.setAttribute(\'value\', this.value);"><input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.previousSibling.querySelector(\'span\').innerHTML = this.value;"></form>';
+				el.innerHTML = '<form onsubmit="return false"><label>Title: <span class="value">1</span>s</label><input id="effect-title" type="text" value="Title" onchange="this.setAttribute(\'value\', this.value);"><input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.parentNode.querySelector(\'span\').innerHTML = this.value;"></form>';
 				stage.dropped(el, 'Title');
 			}
 		});
