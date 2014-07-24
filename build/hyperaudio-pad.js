944ec94732f7d4531f25cb12cb379e7f420ac6bb
Mark Panaghiston <markp@happyworm.com>
2014-07-24 16:06:21 +0100

    Inc pkg and rebuilt

--- build/hyperaudio-pad.js
+++ build/hyperaudio-pad.js
@@ -1,4 +1,4 @@
-/*! hyperaudio-pad *PAD* v0.5.0 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 20th July 2014 18:38:02 */
+/*! hyperaudio-pad v0.5.1 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 24th July 2014 15:56:42 */
 /*! hyperaudio-lib v0.5.0 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 20th July 2014 18:36:29 */
 (function(global, document) {
 
@@ -11303,10 +11303,16 @@ hyperaudio.utility('xhr', xhr); // fn
 }(window, document));
 
 
+/* Hyperaudio Pad */
 
-var HAP = {};
+HAP = (function (window, document, HA) {
 
-HAP.init = (function (window, document) {
+	var HAP = {
+		options: {
+			viewer: false, // True for read only viewer
+			defaultTranscriptId: 'XMVjtXOUSC-V0sSZBOKrBw'
+		}
+	};
 
 	var player;
 	var projector;
@@ -11325,8 +11331,6 @@ HAP.init = (function (window, document) {
 	var fade;
 	var trim;
 	var title;
-
-	var defaultTranscriptId = 'XMVjtXOUSC-V0sSZBOKrBw';
 
 	var transcriptId = HA.getURLParameter('t');
 	var mixId = HA.getURLParameter('m');
@@ -11351,10 +11355,10 @@ HAP.init = (function (window, document) {
 		// Init the Address utility
 		HA.Address.init();
 
-		player = HA.Player({
-			target: "#video-source",
-			gui: true
-		});
+		mixTitleForm = document.getElementById('mix-title-form');
+		mixTitle = document.getElementById('mix-title');
+		saveButton = document.getElementById('save-button');
+		savingAnim = document.querySelector('#save-button-saving');
 
 		music = HA.Music({
 			target: "#music-player"
@@ -11367,215 +11371,225 @@ HAP.init = (function (window, document) {
 
 		stage = HA.Stage({
 			target: "#stage",
-			projector: projector
+			projector: projector,
+			editable: !HAP.options.viewer
 		});
-
-		transcript = HA.Transcript({
-			target: "#transcript",
-			stage: stage,
-			player: player
-		});
-
-		mixTitleForm = document.getElementById('mix-title-form');
-		mixTitle = document.getElementById('mix-title');
-		saveButton = document.getElementById('save-button');
-		savingAnim = document.querySelector('#save-button-saving');
-
-		function mediaSelect (el) {
-			var id = el.getAttribute('data-id');
-			sidemenu.close();
-			transcript.load(id);
-		}
-
-		// Init the side menu
-		sidemenu = new HA.SideMenu({
-			el: '#sidemenu',
-			stage: stage,
-			callback: mediaSelect
-		});
-
-		// Title handler
-		mixTitleHandler = function(e) {
-			e.preventDefault();
-			stage.mixDetails({
-				title: mixTitle.value
-			});
-			if(typeof mixTitle.blur === 'function') {
-				mixTitle.blur();
-			}
-			HA.gaEvent({
-				type: 'HAP',
-				action: 'titlechange: Mix title changed'
-			});
-		};
-
-		// Title
-		mixTitleForm.addEventListener('submit', mixTitleHandler, false);
-		mixTitleForm.addEventListener('change', mixTitleHandler, false);
-		mixTitle.addEventListener('keyup', function(e) {
-			stage.mixDetails({
-				title: this.value
-			});
-		}, false);
 
 		stage.target.addEventListener(HA.event.load, function(e) {
-			mixTitle.value = HA.api.mix.label;
-			notify('load'); // Tell top frame the mix was loaded
-		}, false);
-		stage.target.addEventListener(HA.event.save, function(e) {
-			savingAnim.style.display = 'none';
-			notify('save'); // Tell top frame the mix was saved
-		}, false);
-
-		// Save button
-		saveButton._tap = new HA.Tap({el: saveButton});
-		saveButton.addEventListener('tap', function () {
-			savingAnim.style.display = 'block';
-			stage.save();
+			if(!HAP.options.viewer) {
+				mixTitle.value = HA.api.mix.label;
+				notify('load'); // Tell top frame the mix was loaded
+			} else {
+				mixTitle.innerHTML = HA.api.mix.label;
+			}
 		}, false);
 
-		// Prompt login if attempting to save
-		var signin = document.querySelector('#signin-modal');
+		if(!HAP.options.viewer) {
+			stage.target.addEventListener(HA.event.save, function(e) {
+				savingAnim.style.display = 'none';
+				notify('save'); // Tell top frame the mix was saved
+			}, false);
+		}
 
-		signin.querySelector('.modal-close').addEventListener('click', function(e) {
-			e.preventDefault();
-			signin.style.display = 'none';
-		});
+		if(!HAP.options.viewer) {
+			player = HA.Player({
+				target: "#video-source",
+				gui: true
+			});
 
-		signin.querySelector('#signin-form').addEventListener('submit', function(e) {
-			e.preventDefault();
-			signin.style.display = 'none';
-			savingAnim.style.display = 'block';
-			HA.api.signin({
-				username: signin.querySelector('#username').value,
-				password: signin.querySelector('#password').value
-			}, function(success) {
-				if(success) {
-					// try and save again
-					notify('login'); // Tell top frame the user has logged in.
-					stage.save();
-				} else {
-					// Show the prompt again
-					signin.style.display = 'block';
-					savingAnim.style.display = 'none';
+			transcript = HA.Transcript({
+				target: "#transcript",
+				stage: stage,
+				player: player
+			});
+
+			function mediaSelect (el) {
+				var id = el.getAttribute('data-id');
+				sidemenu.close();
+				transcript.load(id);
+			}
+
+			// Init the side menu
+			sidemenu = new HA.SideMenu({
+				el: '#sidemenu',
+				stage: stage,
+				callback: mediaSelect
+			});
+
+			// Title handler
+			mixTitleHandler = function(e) {
+				e.preventDefault();
+				stage.mixDetails({
+					title: mixTitle.value
+				});
+				if(typeof mixTitle.blur === 'function') {
+					mixTitle.blur();
+				}
+				HA.gaEvent({
+					type: 'HAP',
+					action: 'titlechange: Mix title changed'
+				});
+			};
+
+			// Title
+			mixTitleForm.addEventListener('submit', mixTitleHandler, false);
+			mixTitleForm.addEventListener('change', mixTitleHandler, false);
+			mixTitle.addEventListener('keyup', function(e) {
+				stage.mixDetails({
+					title: this.value
+				});
+			}, false);
+
+			// Save button
+			saveButton._tap = new HA.Tap({el: saveButton});
+			saveButton.addEventListener('tap', function () {
+				savingAnim.style.display = 'block';
+				stage.save();
+			}, false);
+
+			// Prompt login if attempting to save
+			var signin = document.querySelector('#signin-modal');
+
+			signin.querySelector('.modal-close').addEventListener('click', function(e) {
+				e.preventDefault();
+				signin.style.display = 'none';
+			});
+
+			signin.querySelector('#signin-form').addEventListener('submit', function(e) {
+				e.preventDefault();
+				signin.style.display = 'none';
+				savingAnim.style.display = 'block';
+				HA.api.signin({
+					username: signin.querySelector('#username').value,
+					password: signin.querySelector('#password').value
+				}, function(success) {
+					if(success) {
+						// try and save again
+						notify('login'); // Tell top frame the user has logged in.
+						stage.save();
+					} else {
+						// Show the prompt again
+						signin.style.display = 'block';
+						savingAnim.style.display = 'none';
+					}
+				});
+			});
+
+			stage.target.addEventListener(HA.event.unauthenticated, function(e) {
+				// Prompt login
+				signin.style.display = 'block';
+				// Hide saving anim
+				savingAnim.style.display = 'none';
+				notify('unauthenticated'); // Tell top frame the user is not logged in.
+			});
+
+			stage.target.addEventListener(HA.event.change, function(e) {
+				notify('change'); // Tell top frame the mix (may have) changed
+			});
+
+			// Init special fx
+			fade = new HA.DragDrop({
+				handle: '#fadeFX',
+				dropArea: stage.target, 
+				draggableClass: 'draggableEffect',
+				onDragStart: function (e) {
+					HA.addClass(stage.target, 'dragdrop');
+					HA.gaEvent({
+						type: 'HAP',
+						action: 'fadeeffectstartdrag: Start drag of Fade effect'
+					});
+				},
+				onDrop: function (el) {
+					HA.removeClass(stage.target, 'dragdrop');
+					if ( !el ) {
+						return;
+					}
+					el.setAttribute('data-effect', 'fade');
+					el.className += ' effect';
+					el.innerHTML = '<form onsubmit="return false"><label>Fade Effect: <span class="value">1</span>s</label><input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.previousSibling.querySelector(\'span\').innerHTML = this.value;"></form>';
+					stage.dropped(el, 'Fade');
+					HA.gaEvent({
+						type: 'HAP',
+						action: 'fadeeffectdrop: Drop Fade effect on to stage'
+					});
 				}
 			});
-		});
 
-		stage.target.addEventListener(HA.event.unauthenticated, function(e) {
-			// Prompt login
-			signin.style.display = 'block';
-			// Hide saving anim
-			savingAnim.style.display = 'none';
-			notify('unauthenticated'); // Tell top frame the user is not logged in.
-		});
-
-		stage.target.addEventListener(HA.event.change, function(e) {
-			notify('change'); // Tell top frame the mix (may have) changed
-		});
-
-		// Init special fx
-		fade = new HA.DragDrop({
-			handle: '#fadeFX',
-			dropArea: stage.target, 
-			draggableClass: 'draggableEffect',
-			onDragStart: function (e) {
-				HA.addClass(stage.target, 'dragdrop');
-				HA.gaEvent({
-					type: 'HAP',
-					action: 'fadeeffectstartdrag: Start drag of Fade effect'
-				});
-			},
-			onDrop: function (el) {
-				HA.removeClass(stage.target, 'dragdrop');
-				if ( !el ) {
-					return;
+			trim = new HA.DragDrop({
+				handle: '#trimFX',
+				dropArea: stage.target,
+				draggableClass: 'draggableEffect',
+				onDragStart: function (e) {
+					HA.addClass(stage.target, 'dragdrop');
+					HA.gaEvent({
+						type: 'HAP',
+						action: 'trimeffectstartdrag: Start drag of Trim effect'
+					});
+				},
+				onDrop: function (el) {
+					HA.removeClass(stage.target, 'dragdrop');
+					if ( !el ) {
+						return;
+					}
+					el.setAttribute('data-effect', 'trim');
+					el.className += ' effect';
+					el.innerHTML = '<form onsubmit="return false"><label>Trim: <span class="value">1</span>s</label><input id="effect-duration" type="range" value="1" min="0" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.previousSibling.querySelector(\'span\').innerHTML = this.value;"></form>';
+					stage.dropped(el, 'Trim');
+					HA.gaEvent({
+						type: 'HAP',
+						action: 'trimeffectdrop: Drop Trim effect on to stage'
+					});
 				}
-				el.setAttribute('data-effect', 'fade');
-				el.className += ' effect';
-				el.innerHTML = '<form onsubmit="return false"><label>Fade Effect: <span class="value">1</span>s</label><input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.previousSibling.querySelector(\'span\').innerHTML = this.value;"></form>';
-				stage.dropped(el, 'Fade');
-				HA.gaEvent({
-					type: 'HAP',
-					action: 'fadeeffectdrop: Drop Fade effect on to stage'
-				});
-			}
-		});
-
-		trim = new HA.DragDrop({
-			handle: '#trimFX',
-			dropArea: stage.target,
-			draggableClass: 'draggableEffect',
-			onDragStart: function (e) {
-				HA.addClass(stage.target, 'dragdrop');
-				HA.gaEvent({
-					type: 'HAP',
-					action: 'trimeffectstartdrag: Start drag of Trim effect'
-				});
-			},
-			onDrop: function (el) {
-				HA.removeClass(stage.target, 'dragdrop');
-				if ( !el ) {
-					return;
-				}
-				el.setAttribute('data-effect', 'trim');
-				el.className += ' effect';
-				el.innerHTML = '<form onsubmit="return false"><label>Trim: <span class="value">1</span>s</label><input id="effect-duration" type="range" value="1" min="0" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.previousSibling.querySelector(\'span\').innerHTML = this.value;"></form>';
-				stage.dropped(el, 'Trim');
-				HA.gaEvent({
-					type: 'HAP',
-					action: 'trimeffectdrop: Drop Trim effect on to stage'
-				});
-			}
-		});
-
-		title = new HA.DragDrop({
-			handle: '#titleFX',
-			dropArea: stage.target,
-			draggableClass: 'draggableEffect',
-			onDragStart: function (e) {
-				HA.addClass(stage.target, 'dragdrop');
-				HA.gaEvent({
-					type: 'HAP',
-					action: 'titleeffectstartdrag: Start drag of Title effect'
-				});
-			},
-			onDrop: function (el) {
-				HA.removeClass(stage.target, 'dragdrop');
-				if ( !el ) {
-					return;
-				}
-				el.setAttribute('data-effect', 'title');
-				el.className += ' effect';
-				var html = '<form onsubmit="return false">' +
-					'<label>Title: <span class="value">1</span>s</label>' +
-					'<div class="effect-checkboxes"><label for="effect-fullscreen">Full Screen:</label> <input type="checkbox" id="effect-fullscreen" checked onchange="if(this.checked) { this.setAttribute(\'checked\', true); } else { this.removeAttribute(\'checked\'); }"></div>' +
-					'<input id="effect-title" type="text" value="Title" onchange="this.setAttribute(\'value\', this.value);" onkeyup="this.setAttribute(\'value\', this.value);">' +
-					'<input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.parentNode.querySelector(\'span\').innerHTML = this.value;">' +
-					'</form>';
-
-				// el.innerHTML = '<form onsubmit="return false"><label>Title: <span class="value">1</span>s</label><input id="effect-title" type="text" value="Title" onchange="this.setAttribute(\'value\', this.value);"><input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.parentNode.querySelector(\'span\').innerHTML = this.value;"></form>';
-				el.innerHTML = html;
-				stage.dropped(el, 'Title');
-				HA.gaEvent({
-					type: 'HAP',
-					action: 'titleeffectdrop: Drop Title effect on to stage'
-				});
-			}
-		});
-
-		if(transcriptId) {
-			HA.gaEvent({
-				type: 'HAP',
-				action: 'loadtranscript: Load Transcript given in URL param'
 			});
-			transcript.load(transcriptId);
-		} else {
-			HA.gaEvent({
-				type: 'HAP',
-				action: 'loaddefaulttranscript: Load default Transcript'
+
+			title = new HA.DragDrop({
+				handle: '#titleFX',
+				dropArea: stage.target,
+				draggableClass: 'draggableEffect',
+				onDragStart: function (e) {
+					HA.addClass(stage.target, 'dragdrop');
+					HA.gaEvent({
+						type: 'HAP',
+						action: 'titleeffectstartdrag: Start drag of Title effect'
+					});
+				},
+				onDrop: function (el) {
+					HA.removeClass(stage.target, 'dragdrop');
+					if ( !el ) {
+						return;
+					}
+					el.setAttribute('data-effect', 'title');
+					el.className += ' effect';
+					var html = '<form onsubmit="return false">' +
+						'<label>Title: <span class="value">1</span>s</label>' +
+						'<div class="effect-checkboxes"><label for="effect-fullscreen">Full Screen:</label> <input type="checkbox" id="effect-fullscreen" checked onchange="if(this.checked) { this.setAttribute(\'checked\', true); } else { this.removeAttribute(\'checked\'); }"></div>' +
+						'<input id="effect-title" type="text" value="Title" onchange="this.setAttribute(\'value\', this.value);" onkeyup="this.setAttribute(\'value\', this.value);">' +
+						'<input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.parentNode.querySelector(\'span\').innerHTML = this.value;">' +
+						'</form>';
+
+					// el.innerHTML = '<form onsubmit="return false"><label>Title: <span class="value">1</span>s</label><input id="effect-title" type="text" value="Title" onchange="this.setAttribute(\'value\', this.value);"><input id="effect-duration" type="range" value="1" min="0.5" max="5" step="0.1" onchange="this.setAttribute(\'value\', this.value); this.parentNode.querySelector(\'span\').innerHTML = this.value;"></form>';
+					el.innerHTML = html;
+					stage.dropped(el, 'Title');
+					HA.gaEvent({
+						type: 'HAP',
+						action: 'titleeffectdrop: Drop Title effect on to stage'
+					});
+				}
 			});
-			transcript.load(defaultTranscriptId);
+
+			if(transcriptId) {
+				HA.gaEvent({
+					type: 'HAP',
+					action: 'loadtranscript: Load Transcript given in URL param'
+				});
+				transcript.load(transcriptId);
+			} else {
+				HA.gaEvent({
+					type: 'HAP',
+					action: 'loaddefaulttranscript: Load default Transcript'
+				});
+				transcript.load(HAP.options.defaultTranscriptId);
+			}
 		}
 
 		if(mixId) {
@@ -11593,25 +11607,39 @@ HAP.init = (function (window, document) {
 	}
 
 	function notify(type) {
-		var topFrame = window.top;
-		if(typeof topFrame.notify === 'function') {
-			topFrame.notify(type);
-		}
+		try {
+			var topFrame = window.top;
+			if(typeof topFrame.notify === 'function') {
+				topFrame.notify(type);
+			}
+		} catch(error) {}
 	}
 
-	// kickstart
-	function init () {
-		// nothing to do
-	}
-
+/*
 	// Not sure if this findDraggable() function is ever called.
 	function findDraggable (el) {
 		return (/(^|\s)item($|\s)/).test(el.className) ? el : false;
 	}
+*/
 
-	window.addEventListener('load', loaded, false);
 	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
 
-	return init;
+	var pageReady = false;
+	window.addEventListener('load', function() {
+		pageReady = true;
+	}, false);
 
-})(window, document);
+	HAP.init = function(options) {
+
+		this.options = HA.extend({}, this.options, options);
+
+		if(pageReady) {
+			loaded();
+		} else {
+			window.addEventListener('load', loaded, false);
+		}
+	};
+
+	return HAP;
+
+})(window, document, HA);
