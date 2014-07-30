1a8f59b17e95e49132ead0bae123f07b8109681c
Mark Panaghiston <markp@happyworm.com>
2014-07-30 21:30:54 +0100

    Imp ha-lib. Minor version point. Rebuilt.

--- build/hyperaudio-pad.js
+++ build/hyperaudio-pad.js
@@ -1,5 +1,5 @@
-/*! hyperaudio-pad v0.5.9 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 30th July 2014 20:49:32 */
-/*! hyperaudio-lib v0.5.5 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 28th July 2014 20:25:35 */
+/*! hyperaudio-pad v0.6.0 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 30th July 2014 21:30:25 */
+/*! hyperaudio-lib v0.6.0 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 30th July 2014 21:27:30 */
 (function(global, document) {
 
   // Popcorn.js does not support archaic browsers
