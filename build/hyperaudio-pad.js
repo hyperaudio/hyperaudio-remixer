e90001f7fb3d86900e85b9ad27569c54d6064836
Mark Panaghiston <markp@happyworm.com>
2014-07-31 16:55:16 +0100

    Imp ha-lib. API domain opt. Inc pkg and rebuilt.

--- build/hyperaudio-pad.js
+++ build/hyperaudio-pad.js
@@ -1,5 +1,5 @@
-/*! hyperaudio-pad v0.6.0 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 30th July 2014 21:30:25 */
-/*! hyperaudio-lib v0.6.0 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 30th July 2014 21:27:30 */
+/*! hyperaudio-pad v0.6.1 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 31st July 2014 16:54:49 */
+/*! hyperaudio-lib v0.6.1 ~ (c) 2012-2014 Hyperaudio Inc. <hello@hyperaud.io> (http://hyperaud.io) http://hyperaud.io/licensing/ ~ Built: 31st July 2014 16:43:10 */
 (function(global, document) {
 
   // Popcorn.js does not support archaic browsers
@@ -7931,9 +7931,14 @@ var api = (function(hyperaudio) {
 	return {
 		init: function(options) {
 			this.options = hyperaudio.extend({
+
+				// Options used to build the API url. See _updateInternals() to see how the API url is built.
 				protocol: 'http://',
 				org: '', // The organisations namespace / sub-domain. EG. 'chattanooga'
-				api: 'api.hyperaud.io/v1/',
+				api: 'api.', // The sub-domain of the API
+				domain: 'hyperaud.io', // The domain of the API
+				version: '/v1/', // The version of the API.
+
 				// Command syntax
 				transcripts: 'transcripts/',
 				transcripts_filter: '?type=html',
@@ -7981,7 +7986,7 @@ var api = (function(hyperaudio) {
 		},
 		_updateInternals: function() {
 			var namespace = this.options.org ? this.options.org + '.' : '';
-			this.url = this.options.protocol + namespace + this.options.api;
+			this.url = this.options.protocol + namespace + this.options.api + this.options.domain + this.options.version;
 		},
 		callback: function(callback, success) {
 			if(typeof callback === 'function') {
@@ -11368,6 +11373,7 @@ HAP = (function (window, document, HA) {
 	var title;
 
 	var namespace = null;
+	var domain = 'hyperaud.io';
 	var myUrls = {};
 
 	var transcriptId = HA.getURLParameter('t');
@@ -11396,7 +11402,7 @@ HAP = (function (window, document, HA) {
 	}
 
 	function getUrls() {
-		var edit = 'http://' + (namespace ? namespace + '.' : '') + 'hyperaud.io/pad/';
+		var edit = 'http://' + (namespace ? namespace + '.' : '') + domain + '/pad/';
 		var share = edit + 'viewer/';
 		var params = '';
 		if(mixId && transcriptId) {
@@ -11417,13 +11423,18 @@ HAP = (function (window, document, HA) {
 
 	function loaded () {
 		
-		if (document.location.hostname.indexOf('hyperaud') > 0) {
-		  namespace = document.location.hostname.substring(0, document.location.hostname.indexOf('hyperaud') - 1);
+		if (document.location.hostname.indexOf('hyperaud') > -1) {
+			namespace = document.location.hostname.substring(0, document.location.hostname.indexOf('hyperaud') - 1);
+		}
+
+		if (document.location.hostname.indexOf('hyperaud.net') > -1) {
+			domain = 'hyperaud.net';
 		}
 
 		// Init the API utility
 		HA.api.init({
-			org: namespace // The organisations namespace / sub-domain. EG. 'chattanooga'
+			org: namespace, // The organisations namespace / sub-domain. EG. 'chattanooga'
+			domain: domain
 		});
 
 		// Init the Clipboard utility
