f9465f84ae4f6112383d4610641b6ebd3fd2e7ab
Laurian Gridinoc <laurian@gridinoc.name>
2017-06-28 14:43:08 +0300

    s3?

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,26 +1,28 @@
 // @flow
 
 import path from 'path';
+// import info from './package.json';
 
 // import { WDS_PORT } from './src/shared/config'
 const WDS_PORT = 5555;
 
 // import { isProd } from './src/shared/util'
-const isProd = false;
+// const isProd = false;
 
 export default {
   entry: ['./src/loader'],
   output: {
+    // filename: `${info.version}-bundle.js`,
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: isProd ? '/static/' : `http://localhost:${WDS_PORT}/dist/`,
+    // publicPath: isProd ? '/static/' : `http://localhost:${WDS_PORT}/dist/`,
   },
   module: {
     rules: [
       {test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/},
     ],
   },
-  devtool: isProd ? false : 'source-map',
+  devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx'],
   },
