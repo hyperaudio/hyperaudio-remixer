cedc20eac962ce8caeb255e8df2ebd0816c1c60d
Laurian Gridinoc <laurian@gridinoc.name>
2017-07-06 19:38:23 +0300

    travis deploy, webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,19 +1,18 @@
 // @flow
 
 import path from 'path';
+
 // import info from './package.json';
-
 // import { WDS_PORT } from './src/shared/config'
-const WDS_PORT = 5555;
-
 // import { isProd } from './src/shared/util'
 // const isProd = false;
+
+const WDS_PORT = 5555;
 
 export default {
   entry: ['./src/loader'],
   output: {
-    // filename: `${info.version}-bundle.js`,
-    filename: 'bundle.js',
+    filename: 'hyperaudio.js',
     path: path.resolve(__dirname, 'dist'),
     // publicPath: isProd ? '/static/' : `http://localhost:${WDS_PORT}/dist/`,
   },
